24c8c10a80db67f8c8d9db40a420759984b8beb5
Shashank-saurabh123 <saurabh123super@gmail.com>
2024-04-08 02:09:06 +0530

    first set up done using vite and created my own react app

---
+++ 01vitreact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+  //  <Chai/>
+   // here  we can export only one element
+// hence we will make a div
+<>
+<Chai/>
+<h1>hello</h1>
+<h3>I am read, are you?
+</h3>
+</>
+  )
+}
+
+export default App
